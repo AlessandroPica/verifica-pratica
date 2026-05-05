ce80aaa875536dce920407b5fffa1d3ff7c74413
AlessandroPica <160123537+AlessandroPica@users.noreply.github.com>
2026-05-05 11:40:31 +0200

    Update corso_maggiore.php

--- corso_maggiore.php
+++ corso_maggiore.php
@@ -1,1 +1,19 @@
+<?php
+include 'db.php';
+session_start();
+if (!isset($_SESSION['loggedin'])) {
+    header('Location: index.php');
+    exit;
+}
 
+$query = "
+    SELECT I.nome, I.cognome, C.nome_corso, COUNT(IC.id_iscrizione) AS iscritti
+    FROM Istruttori I
+    JOIN Corsi C ON I.id_istruttore = C.id_istruttore
+    JOIN Iscrizioni_Corsi IC ON C.id_corso = IC.id_corso
+    GROUP BY C.id_corso
+    HAVING iscritti >= 5
+    ORDER BY iscritti DESC
+";
+$result = $conn->query($query);
+?>
